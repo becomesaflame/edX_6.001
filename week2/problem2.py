6eae75d2b82a36512100eafae29bbca768ef1fbe
Steve Grandpre <Steven_D_Grandpre@raytheon.com>
2016-10-05 10:22:31 -0400

    Pset2p2 mostly working, but gives wrong answers

--- week2/problem2.py
+++ week2/problem2.py
@@ -43,8 +43,10 @@ if __name__ == "__main__":
   paymentGuess = principal/6
   lowPaymentGuess = principal/12
   highPaymentGuess = principal
-  endOfYearBalance = principal
+  endOfYearBalance = calcAnnualBalance(principal, annualInterestRate, paymentGuess)
   while (endOfYearBalance < closeEnough) or (endOfYearBalance > 0):
+    # print("With payment of %.2f, balance is %.2f" % (paymentGuess, endOfYearBalance))
+    # print("Low: %.2f    High: %.2f" % (lowPaymentGuess, highPaymentGuess))
     if endOfYearBalance > 0.00:
       # increase payment guess
       lowPaymentGuess = paymentGuess
@@ -54,9 +56,8 @@ if __name__ == "__main__":
       highPaymentGuess = paymentGuess
       paymentGuess -= (paymentGuess - lowPaymentGuess)/2
     endOfYearBalance = calcAnnualBalance(principal, annualInterestRate, paymentGuess)
-    print("With payment of %.2f, balance is %.2f" % (paymentGuess, endOfYearBalance))
-    input()
-  print ( "Lowest Payment: " + paymentGuess)
+    # input()
+  print ( "Lowest Payment: %.2f" % paymentGuess)
 
 '''
 balance = float(input('Enter balance: '))
