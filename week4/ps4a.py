b16b1e29277ce3431551a5385ad90fdc1c79426f
Steve Grandpre <Steven_D_Grandpre@raytheon.com>
2016-10-12 20:43:42 -0400

    started ps4p3

--- week4/ps4a.py
+++ week4/ps4a.py
@@ -168,7 +168,10 @@ def isValidWord(word, hand, wordList):
     hand: dictionary (string -> int)
     wordList: list of lowercase strings
     """
-    # TO DO ... <-- Remove this comment when you code this function
+    if word not in wordList:
+        return False
+    
+    freq = getFrequencyDict(word) 
 
 
 #
